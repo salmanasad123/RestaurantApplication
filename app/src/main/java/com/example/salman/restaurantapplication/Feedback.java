887296d1e9d78e389d991e4d8ae8474b3bf59a47
slmanasad <slman.me@gmail.com>
2018-06-20 22:11:35 +0500

    commit1

---
+++ app/src/main/java/com/example/salman/restaurantapplication/Feedback.java
@@ -0,0 +1,80 @@
+package com.example.salman.restaurantapplication;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Salman on 6/20/2018.
+ */
+
+public class Feedback {
+
+    @SerializedName("FeedbackID")
+    @Expose
+    private Integer feedbackID;
+    @SerializedName("Rating")
+    @Expose
+    private Integer rating;
+    @SerializedName("comment")
+    @Expose
+    private String comment;
+    @SerializedName("RestaurantID")
+    @Expose
+    private Integer restaurantID;
+    @SerializedName("created_at")
+    @Expose
+    private String createdAt;
+    @SerializedName("updated_at")
+    @Expose
+    private String updatedAt;
+
+    public Integer getFeedbackID() {
+        return feedbackID;
+    }
+
+    public void setFeedbackID(Integer feedbackID) {
+        this.feedbackID = feedbackID;
+    }
+
+    public Integer getRating() {
+        return rating;
+    }
+
+    public void setRating(Integer rating) {
+        this.rating = rating;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+
+    public Integer getRestaurantID() {
+        return restaurantID;
+    }
+
+    public void setRestaurantID(Integer restaurantID) {
+        this.restaurantID = restaurantID;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+}
+
--- app/src/main/java/com/example/salman/restaurantapplication/FeedbackActivity.java
+++ app/src/main/java/com/example/salman/restaurantapplication/FeedbackActivity.java
@@ -8,23 +8,15 @@ import android.widget.TextView;
 public class FeedbackActivity extends AppCompatActivity {
 
     RatingBar ratingBar;
-    TextView txtRatingValue;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feedback);
-        txtRatingValue = findViewById(R.id.textRatingValue);
+
         ratingBar = findViewById(R.id.feedbackRating);
 
 
-        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-
-                txtRatingValue.setText(String.valueOf(rating));
-            }
-        });
     }
 }
